8c81f1ae71157e1a165287e8c7c91c3d0ba52abc
Omewah <joelomewah@ymail.com>
2023-05-04 12:43:35 -0700

    fix code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,12 +11,12 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	if (b == NULL)
-		return (0);
-
 	unsigned int convert = 0;
 	unsigned int pwr = 1;
 	int lng;
+
+	if (b == NULL)
+		return (0);
 
 	for (lng = 0; b[lng];)
 		lng++;
