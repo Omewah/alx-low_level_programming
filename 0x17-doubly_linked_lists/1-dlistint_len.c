dc150c6399c75bcb59f971e137fe5de14b79ed06
Omewah <joelomewah@ymail.com>
2023-06-15 13:35:44 -0700

    count the number of elememts

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,20 @@
 #include "lists.h"
+
 /**
-* dlistint_len - function
-* @h: pointer to head node of list
-*
-* Description: function to return number of elements in a lnkd list
-* Return: Number of elements
-*/
+ * dlistint_len - returns the number of elements in a dlistint_t list
+ * @h: head of doubly linked list
+ *
+ * Return: number of nodes
+ */
+
 size_t dlistint_len(const dlistint_t *h)
 {
-	int elements = 0;
+	int count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
+		count++;
 		h = h->next;
-		elements++;
 	}
-	return (elements);
+	return (count);
 }
