9449a4b35068e8a7cffebe6db3f6ea540e96ae98
Omewah <joelomewah@ymail.com>
2023-06-15 20:29:12 +0100

    add new node at the end of the list

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end -  add new node at the end of the list
+ * @head: the head pointer of the list
+ * @n: the integer value of the new node
+ * Return: address of new wlwmwnr, Else NULL if failed
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_ele;
+	dlistint_t *end;
+
+	new_ele = malloc(sizeof(dlistint_t));
+	if (new_ele == NULL)
+		return (NULL);
+
+	new_ele->n = n;
+	new_ele->next = NULL;
+
+	if (*head != NULL)
+	{
+		new_ele->prev = NULL;
+		*head = new_ele;
+		return (new_ele);
+	}
+
+	end = *head;
+	while (end->next != NULL)
+		end = end->next;
+	end->next = new_ele;
+	new_ele->prev = end;
+
+	return (new_ele);
+}
