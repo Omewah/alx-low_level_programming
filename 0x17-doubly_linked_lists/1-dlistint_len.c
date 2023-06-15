67315cca1e629d7a2df79596391108f19855a917
Omewah <joelomewah@ymail.com>
2023-06-15 20:14:19 +0100

    returns the number of elements in LL

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in LL
+ * @h: the head of the list
+ * Return: the number of elements in the LL
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
