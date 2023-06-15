1d790c6da4cc5fc3a48925d5749071ebc5703485
Omewah <joelomewah@ymail.com>
2023-06-15 20:40:48 +0100

    sum all data in the list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/**
+ * sum_dlistint - sums all the data of the LL
+ * @head: the head of the list
+ * Return: 0 if the list is empty
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int datasum = 0;
+
+	while (head)
+	{
+		datasum += head->n;
+		head = head->next;
+	}
+
+	return (datasum);
+}
